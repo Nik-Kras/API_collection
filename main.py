6d6169e04b699e6c41e3f6bfd28bcffff47867da
Nikita <nikita.kras.kyiv@gmail.com>
2022-09-26 14:58:17 +0100

    Some API call cases

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 """
+Testing API:
+    1. httpbin.org
+
+Collection API:
+    1. openweathermap.org
+    2. EarthQuakes?
+    3. Flight delay
+    4. National Floods API
+
 response.content    # Return the raw bytes of the data payload
 response.text       # Return a string representation of the data payload
-response.json()     # This method is convenient when the API returns JSON
+response.json()     # This method is convenient when the API returns JSON   (USE)
 """
 
 import requests
+import json
 
 def main():
-    response = requests.get("https://api.github.com/repos/Nik-Kras/API_collection")
-    print(response)
-    print("response.content: ", response.content)
-    print("response.text: ", response.text)
-    print("response.json(): ", response.json())
+
+    response = requests.post('https://httpbin.org/post', data={'key': 'value'})
+    print("POST: ", json.dumps(response.json(), indent=4))
+
+    response = requests.get('https://httpbin.org/get')
+    print("GET: ", json.dumps(response.json(), indent=4))
+
+    response = requests.get('https://httpbin.org/ip')
+    print("IP: ", json.dumps(response.json(), indent=4))
+
+    r = requests.put('https://httpbin.org/put', data={'key': 'value'})
+    print("Put: ", json.dumps(r.json(), indent=4))
+
+    r = requests.delete('https://httpbin.org/delete')
+    print("Delete: ", json.dumps(r.json(), indent=4))
+
+    r = requests.head('https://httpbin.org/get')
+    print("Head: ", r)
+
+    r = requests.options('https://httpbin.org/get')
+    print("Options: ", r)
 
 if __name__ == '__main__':
     main()
