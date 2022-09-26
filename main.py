e065a202b54dd62ee710973fc2e217f962689e9f
Nikita <nikita.kras.kyiv@gmail.com>
2022-09-26 14:33:16 +0100

    test api

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+"""
+response.content    # Return the raw bytes of the data payload
+response.text       # Return a string representation of the data payload
+response.json()     # This method is convenient when the API returns JSON
+"""
+
 import requests
 
 def main():
-    response = requests.get("http://api.open-notify.org/astros.json")
+    response = requests.get("https://api.github.com/repos/Nik-Kras/API_collection")
     print(response)
+    print("response.content: ", response.content)
+    print("response.text: ", response.text)
+    print("response.json(): ", response.json())
 
 if __name__ == '__main__':
     main()
